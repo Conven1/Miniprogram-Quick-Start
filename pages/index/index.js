1fca66dac7d0a67e9ce7d749084993587866cc03
kyle.song <1834206312@qq.com>
2024-08-22 17:27:34 +0800

    config

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,13 @@
 // index.js
-Page({})
+var helloData = {
+    name: 'Weixin'
+}
+Page({
+    data: helloData,
+    changeName: function (e) {
+        // sent data change to view
+        this.setData({
+            name: 'MINA'
+        })
+    }
+})
